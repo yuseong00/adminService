9a8d3db335f932524907e2da7b62e7fbe3d1cb80
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-03-02 17:32:44 +0900

    #30 메인 클래스 케이즈 리팩토링
    Fastcampus->FastCampus

--- fastcampus-project-admin/src/main/java/com/fastcampus/projectadmin/FastCampusProjectAdminApplication.java
+++ fastcampus-project-admin/src/main/java/com/fastcampus/projectadmin/FastCampusProjectAdminApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-public class FastcampusProjectAdminApplication {
+public class FastCampusProjectAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FastcampusProjectAdminApplication.class, args);
+		SpringApplication.run(FastCampusProjectAdminApplication.class, args);
 	}
 
 }
